03e9abc8c18825a8869b11a306e9410f0de0e76d
Mike Cao <mike@mikecao.com>
2022-12-29 23:31:57 -0800

    Updated types.

--- src/client.ts
+++ src/client.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import chalk from 'chalk';
 import debug from 'debug';
+import { PrismaClientOptions, RawValue } from '@prisma/client/runtime';
 
 const log = debug('umami:prisma-client');
 const PRISMA = Symbol();
 
-const PRISMA_OPTIONS = {
+const PRISMA_OPTIONS: PrismaClientOptions = {
   log: [
     {
       emit: 'event',
@@ -18,7 +19,7 @@ function logQuery({ params, query, duration }) {
   log(chalk.yellow(params), '->', query, chalk.greenBright(`${duration}ms`));
 }
 
-function getClient(options) {
+function getClient(options: PrismaClientOptions): PrismaClient {
   const client = new PrismaClient(options);
 
   if (process.env.LOG_QUERY) {
@@ -34,15 +35,21 @@ function getClient(options) {
   return client;
 }
 
-async function rawQuery(query, params = []) {
+async function rawQuery(query: string, params: RawValue[] = []) {
   return prisma.$queryRawUnsafe.apply(prisma, [query, ...params]);
 }
 
-async function transaction(queries) {
-  return prisma.$transaction(queries);
+async function transaction(input: any, options?: any) {
+  return prisma.$transaction(input, options);
 }
 
 // Initialization
-const prisma = global[PRISMA] || getClient(PRISMA_OPTIONS);
+const prisma: PrismaClient = (global[PRISMA] || getClient(PRISMA_OPTIONS)) as PrismaClient;
 
-export default { client: prisma, log, rawQuery, transaction, PRISMA };
+export default { client: prisma, log, rawQuery, transaction, PRISMA } as {
+  client: PrismaClient;
+  log: any;
+  rawQuery: (query: string, params: RawValue[]) => Promise<any>;
+  transaction: (input: any, options?: any) => Promise<any>;
+  PRISMA: symbol;
+};
